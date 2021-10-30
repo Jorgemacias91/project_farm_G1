cd33da55b6792cc2ddf53a126a68de952a88aa7d
Jorge <jmaciasvesga@gmail.com>
2021-10-30 07:53:17 -0500

    avance en el reto 5 con front

--- Backend/src/main/java/RETO3/RETO3/intefaceReservacion.java
+++ Backend/src/main/java/RETO3/RETO3/intefaceReservacion.java
@@ -4,6 +4,10 @@
  */
 package RETO3.RETO3;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -11,5 +15,12 @@ import org.springframework.data.repository.CrudRepository;
  * @author USUARIO
  */
 public interface intefaceReservacion extends CrudRepository<Reservacion,Integer> {
+    
+    public List<Reservacion> findAllByStatus(String status);
+    
+    public List<Reservacion> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+
+    @Query ("SELECT c.client, COUNT(c.client) from Reservacion AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> CountTotalReservationsByClient();
     
 }
